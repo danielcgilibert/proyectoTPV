b5d62ecd25a53c522555aa6ee48d07da2a4b77c9
Daniel Carmona <danielcgilibert@gmail.com>
2021-05-11 21:02:07 +0200

    añadido modal de productos

--- admin.php
+++ admin.php
@@ -3,14 +3,19 @@ require('head.php');
 require_once "./db/database.php";
 
 $mesas = cargar_mesas();
+$categorias = cargar_categorias();
 
+foreach ($categorias as $categoria) {
+  $productosCa[$categoria["nombre"]] = cargar_productos($categoria["idCategoria"]);
+}
 ?>
 
 <body>
   <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
-    <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Aroma Tapas</a>
+    <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="./index.php">Aroma Tapas</a>
 
     <ul class="navbar-nav px-3">
+
       <li class="nav-item text-nowrap">
         <a class="nav-link" href="./index.php">
           <i class="fas fa-sign-out-alt"></i>
@@ -22,8 +27,9 @@ $mesas = cargar_mesas();
 
   <div class="container-fluid">
     <div class="row">
-      <nav class="col-md-2 d-none d-md-block bg-light sidebar pt-5">
+      <nav class="col-md-2 d-none d-md-block bg-light sidebar pt-5" id="mySidebar">
         <div class="sidebar-sticky">
+
           <ul class="nav flex-column menuLateral">
             <li class="nav-item">
               <a class="nav-link active" href="#" id="dashboard-Link">
@@ -89,25 +95,25 @@ $mesas = cargar_mesas();
 
               <a class="nav-link" href="#">
                 <i class="far fa-file"></i>
-                Current month
+                Listado Ejemplo
               </a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#">
                 <i class="far fa-file"></i>
-                Last quarter
+                Listado Ejemplo
               </a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#">
                 <i class="far fa-file"></i>
-                Social engagement
+                Listado Ejemplo
               </a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#">
                 <i class="far fa-file"></i>
-                Year-end sale
+                Listado Ejemplo
               </a>
             </li>
           </ul>
@@ -272,10 +278,186 @@ $mesas = cargar_mesas();
 
         <div id="Categorias">
           <h1>Categorias</h1>
+
+          <div class="row p-5">
+            <div class="col-md-4 mb-3 text-center ">
+              <div class="card">
+                <div class="card-body">
+                  <h5>Crear categoria</h5>
+                  <button type="button" class="btn btn-outline-primary"><i class="fas fa-plus-square"></i></button>
+                </div>
+              </div>
+
+
+            </div>
+
+            <div class="col-md-8 mb-3 text-center">
+              <div class="card">
+                <div class="card-body">
+                  <ul class="list-group list-group-flush">
+                    <li class="list-group-item">
+                      <p><strong>Ultima categoria actualizada</strong> : Arroces</p>
+                    </li>
+                    <li class="list-group-item">
+                      <p><strong>Cantidad de categorias </strong> : <span class="badge badge-info p-2">9</span></p>
+                    </li>
+                  </ul>
+
+                </div>
+              </div>
+
+            </div>
+          </div>
+
+          <hr>
+          <div class="row text-center p-5">
+
+            <?php foreach ($categorias as $categoria) { ?>
+
+              <div class="col-md-3 mb-3 text-center ">
+
+                <div class="card" style="width: 18rem;" idCategoria=<?= $categoria["idCategoria"] ?>>
+                  <img class="card-img-top" src="https://freepikpsd.com/wp-content/uploads/2019/10/food-salad-image-2962-food-png-picture-of-food-png-428_230.png" alt="Card image cap">
+                  <div class="card-body">
+                    <h5 class="card-title"><?= $categoria["nombre"] ?></h5>
+                    <hr>
+                  </div>
+
+                  <div class="card-body">
+                    <div class="row">
+
+                      <div class="col-sm-6">
+                        <button type="button" class="btn btn-outline-info"><i class="fas fa-edit"></i> Editar</button>
+
+                      </div>
+                      <div class="col-sm-6">
+                        <button type="button" class="btn btn-outline-danger"><i class="fas fa-trash-alt"></i> Borrar</button>
+                      </div>
+
+                    </div>
+                  </div>
+                </div>
+              </div>
+            <?php } ?>
+          </div>
+
+
+
+
+
         </div>
 
         <div id="Productos">
-          <h1>Productos</h1>
+          <h1 class="mb-5 text-center">Productos</h1>
+          <?php foreach ($productosCa as $categoria => $productos) { ?>
+
+
+            <?php if (count($productos)) {
+              echo '<h2>'  . $categoria  . '</h2>';
+              echo '<hr/>';
+            } ?>
+
+            <div class="row text-center">
+
+              <?php
+              foreach ($productos as $producto) { ?>
+
+                <div class="col-md-3 mb-2 text-center p-3 producto" idProducto=<?= $producto["idProducto"] ?>>
+
+                  <div class="card" style="width: 18rem;" idCategoria=<?= $producto["idCategoria"] ?>>
+                    <img class="card-img-top" src="https://freepikpsd.com/wp-content/uploads/2019/10/food-salad-image-2962-food-png-picture-of-food-png-428_230.png" alt="Card image cap">
+                    <div class="card-body">
+                      <h5 class="card-title nombreProducto"><?= $producto["nombre"] ?></h5>
+                      <hr>
+
+                      <p class="descripcionProducto"> <?= $producto["descripcion"] ?> </p>
+                      <p class="precioProducto"> <?= $producto["precio"] ?> € </p>
+
+                    </div>
+
+                    <div class="p-5">
+                      <div class="row">
+
+                        <div class="col-sm-6">
+                          <button type="button" class="btn btn-outline-info"><i class="fas fa-edit"></i> Editar</button>
+
+                        </div>
+                        <div class="col-sm-6">
+                          <button type="button" class="btn btn-outline-danger"><i class="fas fa-trash-alt"></i> Borrar</button>
+                        </div>
+
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+              <?php } ?>
+
+            </div>
+
+
+
+          <?php } ?>
+
+
+
+          <div class="modal fade" id="modalProducto" role="dialog">
+            <div class="modal-dialog">
+
+              <!-- Modal content-->
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h4 class="modal-title"><i class="far fa-folder-open"></i> Producto</h4>
+                  <button type="button" class="close" data-dismiss="modal">&times;</button>
+                </div>
+                <div class="modal-body">
+                  <form>
+                    <div class="form-row">
+                      <div class="form-group col-md-6">
+                        <label for="inputIdProducto"><i class="far fa-list-alt"></i>ID Producto</label>
+                        <input type="number" class="form-control text-center" id="inputIdProducto" disabled=true>
+                      </div>
+
+                      <div class="form-group col-md-6">
+                        <label for="inputNombreProducto"><i class="far fa-edit"></i>Nombre Producto</label>
+                        <input type="text" class="form-control" id="inputNombreProducto">
+                      </div>
+
+                    </div>
+
+                    <div class="form-row">
+                      <div class="form-group col-md-12">
+                        <label for="inputNombreProducto"><i class="far fa-file-alt"></i>Descripción</label>
+                        <textarea class="form-control" id="inputDescripcionProducto" rows="3"></textarea>
+                      </div>
+                    </div>
+
+
+                    <div class="form-row">
+                      <div class="form-group col-md-8">
+                      </div>
+                      <div class="form-group col-md-4">
+                        <label for="inputPrecioProducto"> <i class="far fa-money-bill-alt"></i>
+                          Precio</label>
+                        <input type="number" class="form-control text-center" id="inputPrecioProducto">
+                      </div>
+                    </div>
+
+                  </form>
+
+                </div>
+                <div class="modal-footer">
+                <button type="button" class="btn btn-success">Guardar</button>
+
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+
+                </div>
+              </div>
+
+            </div>
+          </div>
+
+
         </div>
 
 
@@ -331,13 +513,10 @@ $mesas = cargar_mesas();
           <div class="row">
 
             <?php
-            // echo "<pre>";
-            // print_r($mesas);
-            // echo "</pre>";
 
             foreach ($mesas as $mesa) { ?>
               <div class="col-sm-4">
-                <div class="mesa">
+                <div class="mesa" mesaEstado=<?= $mesa["estado"] ?> mesaId=<?= $mesa["idMesa"] ?> ticketId=<?= $mesa["idTicket"] ?>>
                   <?= $mesa["estado"] == 0 ? '<span class="badge badge-success">' : '<span class="badge badge-danger">' ?>
                   <strong>
                     <p>Mesa <?= $mesa["idMesa"] ?></p>
@@ -370,11 +549,98 @@ $mesas = cargar_mesas();
         <!-- Modal content-->
         <div class="modal-content">
           <div class="modal-header">
+            <h4 class="modal-title">Ticket</h4>
             <button type="button" class="close" data-dismiss="modal">&times;</button>
-            <h4 class="modal-title">Modal Header</h4>
           </div>
           <div class="modal-body">
+            <form>
+              <div class="form-group">
+                <div class="row">
+                  <div class="col-md-9">
+                    <label type="text"> <i class="fas fa-clipboard-list"></i> <strong> Número de ticket </strong> </label>
+                  </div>
+                  <div class="col-md-3">
+                    <label type="text"> <strong> 4 </strong></label>
+                  </div>
 
+                </div>
+                <hr>
+              </div>
+
+              <div class="form-group">
+                <div class="row mb-1">
+                  <div class="col-md-7">
+                    <label type="text"> <i class="fas fa-chalkboard-teacher"></i> <strong> Atendido por </strong> </label>
+                  </div>
+                  <div class="col-md-5">
+                    <label type="text"> Manuel Sanchez Ortiz</label>
+                  </div>
+                </div>
+                <div class="row  mb-1">
+                  <div class="col-md-9 ">
+                    <label type="text"> <i class="fas fa-chair"></i>
+                      <strong> En la mesa </strong> </label>
+                  </div>
+                  <div class="col-md-3">
+                    <label type="text"> 1 </label>
+                  </div>
+                </div>
+
+                <div class="row mb-2">
+                  <div class="col-md-7">
+                    <label type="text"><i class="far fa-clock"></i>
+                      <strong> Fecha </strong> </label>
+                  </div>
+                  <div class="col-md-5 ">
+                    <label type="text"> 20/03/2019 : 14:45 </label>
+                  </div>
+                </div>
+
+                <hr>
+
+
+                <div class="row">
+                  <div class="col-md-8 ">
+                    <label type="text"><i class="fas fa-briefcase"></i>
+                      <strong> Empresa </strong> </label>
+                  </div>
+
+                  <div class="col-md-3 text-center ">
+                    <strong>CIF 2452 </strong></label>
+                  </div>
+
+
+                </div>
+
+                <div class="row ">
+                  <div class="col-md-7 ">
+                    <label type="text"> <strong> - Nombre </strong> : </label>
+                  </div>
+                  <div class="col-md-5 text-center ">
+                    <label type="text"> Aroma tapas EP</label>
+                  </div>
+                </div>
+
+                <div class="row ">
+                  <div class="col-md-8 ">
+                    <label type="text"> <strong> - Telefono</strong> : </label>
+                  </div>
+                  <div class="col-md-3 text-center">
+                    <label type="text"> 242424</label>
+                  </div>
+                </div>
+
+                <hr>
+                <div class="row ">
+                  <div class="col-md-12 ">
+                    <label type="text"> <i class="fas fa-receipt"></i> <strong>Cuenta </strong></label>
+                  </div>
+                </div>
+
+              </div>
+
+
+            </form>
 
           </div>
           <div class="modal-footer">
--- js/dashboard.js
+++ js/dashboard.js
@@ -5,16 +5,42 @@ $(document).ready(function () {
       $(".nav-link").removeClass("active");
 
       const clase = target.id.split("-"); // busca el id de los div para mostrar
-      
-      $("#" + target.id).addClass("active");
-        console.log(target.id);
-      $("#" + clase[0]).show("slow");
 
-    
+      $("#" + target.id).addClass("active");
+      $("#" + clase[0]).show();
     }
   });
 
-  $(".mesa").on("click", function () {
+  $(".mesa").on("click", function ({ target }) {
+    const padre = $(target).closest(".mesa");
+    let mesaid = padre.attr("mesaid");
+    let ticketid = padre.attr("ticketid");
+    let mesaEstado = padre.attr("mesaEstado");
+
     $("#modalMesa").modal();
+  });
+
+  $(".producto").on("click", function ({ target }) {
+    const padre =  $(target).closest(".producto");
+
+    console.log(padre);
+    const IdProducto = padre.attr("idproducto");
+    const nombreProducto = padre.find(".nombreProducto").text();
+    const descripcionProducto = padre.find(".descripcionProducto").text();
+    const precioProducto = padre.find(".precioProducto").text();
+
+
+   const inputIdProducto=  $("#modalProducto #inputIdProducto");
+   const inputNombreProducto=  $("#modalProducto #inputNombreProducto");
+   const inputDescripcionProducto=  $("#modalProducto #inputDescripcionProducto");
+   const inputPrecioProducto = $("#modalProducto #inputPrecioProducto");
+   
+
+   inputIdProducto[0].value = IdProducto;
+   inputNombreProducto[0].value = nombreProducto;
+   inputDescripcionProducto[0].value = descripcionProducto;
+   inputPrecioProducto[0].value = parseInt(precioProducto);
+
+    $("#modalProducto").modal();
   });
 });
